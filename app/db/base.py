be98d370c9172a03de8b1af2826cd363fc794b45
ss-o-furda <orik7800@gmail.com>
2022-08-01 03:22:22 +0300

    Import models to base file

---
+++ app/db/base.py
@@ -0,0 +1,4 @@
+from models.comments import Comments, Replies
+from models.images import Avatars, Images
+from models.posts import Likes, PostImages, Posts
+from models.users import Followers, Users
